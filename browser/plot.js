c4d977c91eb7bfd5e3dc9c9bd9ee5b51b7cd2ffe
Jasper <wellcaffeinated@iCoffee.local>
2013-04-29 18:16:15 -0400

    added random data plot

--- browser/plot.js
+++ browser/plot.js
@@ -21,7 +21,31 @@
 
         plotData: function( data ){
 
+            var l = data.length
+                ,rows = Math.ceil(Math.sqrt( data.length ))
+                ,cols = rows
+                ;
+
             // todo
+            for ( var i = 0; i < l; ++i ){
+                
+                this.drawPoint( i % cols, Math.floor(i / cols), data[ i ] );
+            }
+        },
+
+        drawPoint: function( x, y, val ){
+
+            var ctx = this.ctx
+                ,color = this.getColorFromVal( val )
+                ;
+
+            ctx.fillStyle = 'rgba('+ color.join(',') + ')';
+            ctx.fillRect( x, y, 1, 1 );
+        },
+
+        getColorFromVal: function( val ){
+
+            return [ (255 * val)|0, 0, (255 * (1-val))|0, 1 ];
         }
     };
 
@@ -32,13 +56,22 @@
             .append('div')
             .classed('plot', true)
             .style('width', width+'px')
+            .style('height', height+'px')
             .append( 'canvas' )
             ;
 
-        var plot = new HeatMap(canvas.node(), {
+        var hm = new HeatMap(canvas.node(), {
             width: width,
             height: height
         });
+
+        var fakeData = new Float64Array( width * height );
+        for ( var i = 0, l = fakeData.length; i < l; ++i ){
+            
+            fakeData[ i ] = Math.random();
+        }
+
+        hm.plotData( fakeData );
     }
 
     createPlot(500, 500);
