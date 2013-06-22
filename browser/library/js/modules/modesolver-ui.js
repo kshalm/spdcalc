3003f7056957945c4aaf0e0af22600678769061f
Jasper <wellcaffeinated@iCoffee.local>
2013-06-22 19:57:26 -0400

    fixes #52

--- browser/library/js/modules/modesolver-ui.js
+++ browser/library/js/modules/modesolver-ui.js
@@ -168,99 +168,101 @@ define(
             refreshSignalFWHMCircle: function( W, X0, Y0, props ){
 
                 var self = this;
-                var circleFWHM = self.plot2dSignal.svg.selectAll("circle").data([0]);
-                var circleoneoveresquared = self.plot2dSignal.svg.selectAll("circle").data([0]);
+                var deg = 180 / Math.PI;
 
+                var data = [ 
+                    { X0: X0 * deg, Y0: Y0 * deg, r: W/2 * deg, title: 'Half width' },
+                    { X0: X0 * deg, Y0: Y0 * deg, r: 1.699 * W/2 * deg, title: '1/e^2' },
+                ];
                 var xx = self.plot2dSignal.scales.x;
                 var yy = self.plot2dSignal.scales.y;
-                var oneoveresquared = 1.699 *W;
 
-
-                circleFWHM.enter()
-                    .append('circle')
-                    .attr("r", Math.abs(xx(X0+W/2) - xx(X0))*180/Math.PI)
-                    .attr("transform", "translate(" + this.heatmapmargins.left +',' + this.heatmapmargins.top +")")
-                    .style("fill", 'transparent')
+                var extension = 0.15; // percentage of yrange to extend label line through
+                var overlays = self.plot2dSignal.svg.selectAll('.overlay').data( data );
+                
+                var overlaysEnter = overlays.enter()
+                    .append('g')
+                    // translate the whole group to correct for margins
+                    .attr('transform', 'translate(' + this.heatmapmargins.left +',' + this.heatmapmargins.top +')')
+                    .attr('class', 'overlay')
+                    ;
+                
+                // create the circle on new data
+                overlaysEnter.append('circle')
+                    .style('fill', 'transparent')
                     .style('stroke', '#1ABC9C')
                     .style('stroke-width', 2)
-                    .style('stroke-opacity', 0.6);
+                    .style('stroke-opacity', 0.6)
+                    ;
 
-
-                circleFWHM.attr('cx', function(d) {
-                        // console.log(xx(X0 * 180/Math.PI), yy(Y0 * 180/Math.PI), X0*180/Math.PI, self.plot2dSignal.scales.x( X0*180/Math.PI ));
-                        return xx(X0 * 180/Math.PI);
-                    });
-
-                circleFWHM.attr('cy', function(d) {
-                        return self.plot2dSignal.scales.y( Y0*180/Math.PI );
-                    });
-
-
-                // circleoneoveresquared = circleFWHM;
-
-                // circleoneoveresquared
-                //     .attr("r", Math.abs(xx(X0+oneoveresquared/2) - xx(X0))*180/Math.PI)
-                //     .style('stroke-opacity', 0.3);
-                    
-                circleoneoveresquared.enter()
-                    .append('circle')
-                    .attr("r", Math.abs(xx(X0+oneoveresquared/2) - xx(X0))*180/Math.PI)
-                    .attr("transform", "translate(80, 60)")
-                    .style("fill", 'transparent')
+                // create lines
+                overlaysEnter.append('line')
                     .style('stroke', '#1ABC9C')
                     .style('stroke-width', 2)
-                    .style('stroke-opacity', 0.3);
+                    .style('stroke-opacity', 1.0)
+                    ;
 
+                // create text
+                overlaysEnter.append('text')
+                    .attr('text-anchor', 'middle')
+                    ;
 
-                circleoneoveresquared.attr('cx', function(d) {
-                        return xx(X0 * 180/Math.PI);
-                    });
+                // set line dimensions based on data
+                overlays.selectAll('line')
+                    .attr('x1', function( d ){
+                        return xx( d.X0 );
+                    })
+                    .attr('y1', function( d, a, idx ){
+                        // naive alternating of direction...
+                        var sign = (idx % 2 ? -1 : 1);
+                        return xx( d.Y0 + sign * d.r );
+                    })
+                    .attr('x2', function( d ){
+                        return xx( d.X0 );
+                    })
+                    .attr('y2', function( d, a, idx ){
+                        var sign = (idx % 2 ? -1 : 1);
+                        return xx( d.Y0 + sign * d.r) - sign * (yy.range()[1] - yy.range()[0]) * extension;
+                    })
+                    ;
 
-                circleoneoveresquared.attr('cy', function(d) {
-                        return self.plot2dSignal.scales.y( Y0*180/Math.PI );
-                    });
+                // update text properties based on data
+                overlays.selectAll('text')
+                    .attr('x', function( d ){
+                        return xx( d.X0 );
+                    })
+                    .attr('y', function( d, a, idx ){
+                        var sign = (idx % 2 ? -1 : 1);
+                        return xx( d.Y0 + sign * d.r) - sign * (yy.range()[1] - yy.range()[0]) * extension;
+                    })
+                    .attr('dx', 0)
+                    .attr('dy', function( d, a, idx ){
+                        var sign = (idx % 2 ? -1 : 1);
+                        return 10 * sign;
+                    })
+                    .text(function( d ){
+                        return d.title;
+                    })
+                    ;
 
-                // Now draw labels on graph 
-                var FWHMLabel = self.plot2dSignal.svg.selectAll("text").data([0]);
-                var oneoveresquaredLabel = self.plot2dSignal.svg.selectAll("text").data([20]);
+                // update the circle based on data
+                overlays.selectAll('circle')
+                    .attr('r', function( d ){ 
 
-                oneoveresquaredLabel.enter()
-                    // .append("text")
-                    // .attr("y", 0)
-                    // .attr("dy", 20)
-                    // .attr("x", 200/2)
-                    // .attr("dx", 50)
-                    // .style("text-anchor", "middle")
-                    // .text( 'helloworld' )
-                    // ;
-                    .append('text')
-                    .attr("x",  xx(X0 * 180/Math.PI))
-                    .attr("transform", "translate(80, 60)")
-                    .attr("y",  self.plot2dSignal.scales.y( Y0*180/Math.PI ))
-                    .attr("dx", 60)
-                    .attr("dy", 20)
-                    .attr("text-anchor", "middle")
-                    .text('Signal 1/e^2')
-                    .attr('fill',  '#1ABC9C');
-                    // 
-               
-                // circle.exit().remove();
-                
-                // //Add lines
-                var line1 = self.plot2dSignal.svg.selectAll('line').data([ 0 ]);
+                        return Math.abs( xx(d.X0 + d.r) - xx(d.X0) ); 
+                    })
+                    .attr('cx', function( d ) {
 
-                // // create
-                line1.enter()
-                    .append('line')
-                    .attr("x1", 200)
-                    .attr("y1", 200)
-                    .attr("x2", 300)
-                    .attr("y2", 200)
-                    .style('stroke-width', 2)
-                    .style('stroke-opacity', 1.0);
+                        return xx( d.X0 );
+                    })
+                    .attr('cy', function( d ) {
 
-                // line.exit().remove();
-                
+                        return yy( d.Y0 );
+                    })
+                    ;
+
+                // remove the overlays when data removed
+                overlays.exit().remove();
 
             },
 
