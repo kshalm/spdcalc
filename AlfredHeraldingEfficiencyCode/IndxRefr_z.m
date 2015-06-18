c97c00acbd0f06c9c9022db2c27974f2495129ab
Krister Shalm <kshalm@gmail.com>
2015-06-18 09:22:07 -0600

    cleaned up files

--- AlfredHeraldingEfficiencyCode/IndxRefr_z.m
+++
@@ -1,4 +0,0 @@
-function nz=IndxRefr_z(lambda)
-
-nz=sqrt(1.9446+ (1.3617.*lambda.^2)./(lambda.^2-0.047)-0.01491.*lambda.^2);
-
--- AlfredHeraldingEfficiencyCode/k_y.m
+++
@@ -1,4 +0,0 @@
-function kk=k_y(lam)
-
-ny=IndxRefr_y(lam);
-kk=ny.*(2*pi)./lam;
