d42702bc5739efb8e66bab9bb7869cbe60c78560
Jasper <jasper.palfree@colorado.edu>
2019-06-06 13:38:46 -0600

    finally

--- spdcalc/src/crystals/mod.rs
+++ spdcalc/src/crystals/mod.rs
@@ -10,29 +10,42 @@ pub mod bibo_1;
 
 /// The type of crystal
 #[derive(Debug)]
-pub enum CrystalType {
-  BBO1,
+#[allow(non_camel_case_types)]
+pub enum Crystals {
+  BBO_1,
   KTP,
 }
 
-/// Get the crystal refraction indicies for specified crystal type
-pub fn get_crystal_indicies(
-  crystal_type :CrystalType,
-  wavelength :f64,
-  temperature :f64
-) -> Indicies {
-  match crystal_type {
-    CrystalType::BBO1 => bbo_1::get_indicies( wavelength, temperature ),
-    CrystalType::KTP => ktp::get_indicies( wavelength, temperature ),
+impl Crystals {
+  /// Get the crystal refraction indicies for this crystal
+  ///
+  /// ## Example
+  /// ```
+  /// use spdcalc::Crystals;
+  /// use spdcalc::crystals::Indicies;
+  /// let crystal = Crystals::BBO_1;
+  /// let nm = 1e-9;
+  /// let indicies = crystal.get_indicies( 720.0 * nm, 293.0 );
+  /// let expected = Indicies(1.6607191519167868, 1.6607191519167868, 1.5420245834707935);
+  /// assert_eq!(indicies, expected)
+  pub fn get_indicies(
+    &self,
+    wavelength :f64,
+    temperature :f64
+  ) -> Indicies {
+    match &self {
+      Crystals::BBO_1 => bbo_1::get_indicies( wavelength, temperature ),
+      Crystals::KTP => ktp::get_indicies( wavelength, temperature ),
+    }
   }
-}
 
-/// Get the crystal meta information for specified crystal type
-pub fn get_crystal_meta(
-  crystal_type :CrystalType
-) -> CrystalMeta {
-  match crystal_type {
-    CrystalType::BBO1 => bbo_1::META,
-    CrystalType::KTP => ktp::META,
+  /// Get the crystal meta information for specified crystal type
+  pub fn get_meta(
+    &self
+  ) -> CrystalMeta {
+    match &self {
+      Crystals::BBO_1 => bbo_1::META,
+      Crystals::KTP => ktp::META,
+    }
   }
 }
--- spdcalc/src/lib.rs
+++ spdcalc/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod junk;
 pub mod crystals;
+pub use crystals::Crystals;
