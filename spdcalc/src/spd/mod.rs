a265fcd46232ddf39b48986abf1939db38fd33d5
Jasper <jasper.palfree@colorado.edu>
2019-06-27 15:47:43 -0600

    better del k test

--- spdcalc/src/spd/mod.rs
+++ spdcalc/src/spd/mod.rs
@@ -21,8 +21,8 @@ pub fn calc_delta_k(
   let r_i = idler.get_direction();
 
   let ns_by_ls = *(ucum::M * signal.get_index(&crystal_setup) / signal.get_wavelength());
-  let ni_by_li = *(ucum::M * idler.get_index(&crystal_setup) / idler.get_wavelength());
-  let np_by_lp = *(ucum::M * pump.get_index(&crystal_setup) / pump.get_wavelength());
+  let ni_by_li = *(ucum::M * idler.get_index(&crystal_setup)  / idler.get_wavelength());
+  let np_by_lp = *(ucum::M * pump.get_index(&crystal_setup)   / pump.get_wavelength());
 
   let mut dk =
       r_s.as_ref() * ns_by_ls
@@ -63,8 +63,8 @@ mod tests {
       temperature : from_celsius_to_kelvin(20.0),
     };
 
-    let signal = Photon::new(PhotonType::Signal, 2.0 * DEG, 3.0 * DEG, wavelength, waist);
-    let idler = Photon::new(PhotonType::Idler, 182. * DEG, 0.05132276669556576 * RAD, wavelength, waist);
+    let signal = Photon::new(PhotonType::Signal, 15. * DEG, 10. * DEG, wavelength, waist);
+    let idler = Photon::new(PhotonType::Idler, 195. * DEG, 0.16946290635813477 * RAD, wavelength, waist);
     let pump = Photon::new(PhotonType::Pump, 0. * DEG, 0. * DEG, 775. * NANO * M, waist);
 
     (crystal_setup, signal, idler, pump)
@@ -73,15 +73,19 @@ mod tests {
   #[test]
   fn calc_delta_k_test() {
     let (crystal_setup, signal, idler, pump) = init();
-    let expected = na::Vector3::new(6908.816094920548, 241.26117431161828, -358.7018265926745);
+    let expected = na::Vector3::new(30851.482867892322, 8266.62991975434, 186669.00855689016);
     let pp = PeriodicPolling{
       period: 0.00004656366863331685 * ucum::M,
       sign: Sign::POSITIVE,
     };
 
+    // println!("{}, {}", signal.get_direction().as_ref(), idler.get_direction().as_ref());
+    // println!("{}, {}, {}", signal.get_index(&crystal_setup), idler.get_index(&crystal_setup), pump.get_index(&crystal_setup));
+
     let del_k = *(calc_delta_k(&signal, &idler, &pump, &crystal_setup, Option::Some(pp)) / ucum::J / ucum::S);
-    assert!(approx_eq!(f64, del_k.x, expected.x, ulps = 2, epsilon = 1e-6), "actual: {}, expected: {}", del_k.x, expected.x);
-    assert!(approx_eq!(f64, del_k.y, expected.y, ulps = 2, epsilon = 1e-6), "actual: {}, expected: {}", del_k.y, expected.y);
-    assert!(approx_eq!(f64, del_k.z, expected.z, ulps = 2, epsilon = 1e-6), "actual: {}, expected: {}", del_k.z, expected.z);
+    // println!("{}", del_k);
+    assert!(approx_eq!(f64, del_k.x, expected.x, ulps = 2, epsilon = 1e-9), "actual: {}, expected: {}", del_k.x, expected.x);
+    assert!(approx_eq!(f64, del_k.y, expected.y, ulps = 2, epsilon = 1e-9), "actual: {}, expected: {}", del_k.y, expected.y);
+    assert!(approx_eq!(f64, del_k.z, expected.z, ulps = 2, epsilon = 1e-9), "actual: {}, expected: {}", del_k.z, expected.z);
   }
 }
