5ef0b94b478e43443bc51d7efadc3d21faf766c0
Jasper <jasper.palfree@colorado.edu>
2020-02-26 15:44:56 -0700

    trying to get examples compling with sphinx

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -67,7 +67,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**/.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -81,6 +81,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_extra_path = ['examples/iframe_figures']
 
 source_suffix = {
     '.rst': 'restructuredtext',
