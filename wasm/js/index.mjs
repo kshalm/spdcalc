ac3cbffd3fbbe9d5b291ca5e4bbd994bfd182f9b
Jasper <well.caffeinated@gmail.com>
2023-04-13 13:55:43 -0600

    wasm wip

--- wasm/js/index.mjs
+++ wasm/js/index.mjs
@@ -5,31 +5,38 @@ import { Spdc } from "./package/src/bindings/spdcalc/spdcalc.js"
 
 async function main() {
   const spdcalc = await bindings.spdcalc()
-  const spdc = Spdc.fromConfig(spdcalc, {
-    "crystal": {
-      "name": "BBO_1",
-      "pm_type": "e->eo",
-      "phi_deg": 0,
-      "theta_deg": 0,
-      "length_um": 2000,
-      "temperature_c": 20
-    },
-    "pump": {
-      "wavelength_nm": 775,
-      "waist_um": 100,
-      "bandwidth_nm": 5.35,
-      "average_power_mw": 1
-    },
-    "signal": {
-      "wavelength_nm": 1550,
-      "phi_deg": 0,
-      "theta_external_deg": 0,
-      "waist_um": 100,
-      "waist_position_um": "auto"
-    },
-    "idler": "auto",
-    "deff_pm_per_volt": 1
-  })
+
+  console.log(Spdc.getConfig(spdcalc))
+
+  const spdc = spdcalc.getSpdc().val
+  console.log(spdc)
+  console.log(spdc.test())
+
+  // const spdc = Spdc.fromConfig(spdcalc, {
+  //   "crystal": {
+  //     "name": "BBO_1",
+  //     "pm_type": "e->eo",
+  //     "phi_deg": 0,
+  //     "theta_deg": 0,
+  //     "length_um": 2000,
+  //     "temperature_c": 20
+  //   },
+  //   "pump": {
+  //     "wavelength_nm": 775,
+  //     "waist_um": 100,
+  //     "bandwidth_nm": 5.35,
+  //     "average_power_mw": 1
+  //   },
+  //   "signal": {
+  //     "wavelength_nm": 1550,
+  //     "phi_deg": 0,
+  //     "theta_external_deg": 0,
+  //     "waist_um": 100,
+  //     "waist_position_um": "auto"
+  //   },
+  //   "idler": "auto",
+  //   "deff_pm_per_volt": 1
+  // })
 
 }
 
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -9,8 +9,26 @@ impl spdcalc::Spdc for Spdc {
   fn from_config(config: spdcalc::SpdcConfig) -> Result<Handle<Self>, spdcalc::Error> {
     Ok(Handle::new(Self))
   }
+  fn get_config() -> spdcalc::CrystalConfig {
+    spdcalc::CrystalConfig {
+      name: "test".into(),
+      pm_type: "test".into(),
+      phi_deg: 0.1,
+      theta_deg: Some(spdcalc::AutoNum::F32(0.2)),
+      length_um: 0.3,
+      temperature_c: 20.
+    }
+  }
+
+  fn test(&self) -> f32 {
+    0.4
+  }
 }
 
 pub struct Spdcalc;
 
-impl spdcalc::Spdcalc for Spdcalc {}
+impl spdcalc::Spdcalc for Spdcalc {
+  fn get_spdc() -> Result<Handle<Spdc>, spdcalc::Error> {
+    Ok(Handle::new(Spdc))
+  }
+}
