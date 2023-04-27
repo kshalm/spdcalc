1ee22705b08137578a3048e58ccc4ca0059cd916
Jasper <well.caffeinated@gmail.com>
2023-04-27 16:07:12 -0600

    finally work around for wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,34 +1,34 @@
 use ::spdcalc::*;
-use wai_bindgen_rust::Handle;
+// use wai_bindgen_rust::Handle;
 
 wai_bindgen_rust::export!("spdcalc.wai");
 
-pub struct Spdc;
-
-impl spdcalc::Spdc for Spdc {
-  fn from_config(config: spdcalc::SpdcConfig) -> Result<Handle<Self>, spdcalc::Error> {
-    Ok(Handle::new(Self))
-  }
-  fn get_config() -> spdcalc::CrystalConfig {
-    spdcalc::CrystalConfig {
-      name: "test".into(),
-      pm_type: "test".into(),
-      phi_deg: 0.1,
-      theta_deg: Some(spdcalc::AutoNum::F32(0.2)),
-      length_um: 0.3,
-      temperature_c: 20.
+impl From<::spdcalc::AutoCalcParam<f64>> for crate::spdcalc::AutoNum {
+  fn from(param: ::spdcalc::AutoCalcParam<f64>) -> Self {
+    match param {
+      ::spdcalc::AutoCalcParam::Auto(_) => crate::spdcalc::AutoNum::String("auto".to_string()),
+      ::spdcalc::AutoCalcParam::Param(x) => crate::spdcalc::AutoNum::F32(x as f32),
     }
   }
+}
 
-  fn test(&self) -> f32 {
-    0.4
+impl From<::spdcalc::CrystalConfig> for crate::spdcalc::CrystalConfig {
+  fn from(config: ::spdcalc::CrystalConfig) -> Self {
+    spdcalc::CrystalConfig {
+      name: config.name.to_string(),
+      pm_type: config.pm_type.to_string(),
+      phi_deg: config.phi_deg as f32,
+      theta_deg: Some(config.theta_deg.into()),
+      length_um: config.length_um as f32,
+      temperature_c: config.temperature_c as f32
+    }
   }
 }
 
 pub struct Spdcalc;
 
-impl spdcalc::Spdcalc for Spdcalc {
-  fn get_spdc() -> Result<Handle<Spdc>, spdcalc::Error> {
-    Ok(Handle::new(Spdc))
+impl crate::spdcalc::Spdcalc for Spdcalc {
+  fn get_config() -> spdcalc::CrystalConfig {
+    ::spdcalc::CrystalConfig::default().into()
   }
 }
